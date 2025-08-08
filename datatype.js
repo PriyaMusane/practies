92016d3a24d6034d0ab040991a5a93d590a53591
PriyaMusane <priyamusane945@gmail.com>
2025-08-08 19:31:27 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,53 @@
+x=10
+y=5
+
+console.log("Addition of x and y is",x+y)
+console.log("difference:",x-y)
+
+favoriteMovie ="War2"
+message = "I Love to Watch" + favoriteMovie
+console.log(message)
+
+
+isHungry = true
+if(isHungry){
+    console.log("Healthy khana; khana hai") //this print when true//
+}else{
+    console.log("Continue with your work...")//this print when false//
+}
+
+let FavoriteAnimal={
+    name: "BUddy",
+    type: "dog",
+    age: 3,
+    isFriendly: true
+};
+
+let mystery;
+console.log("Value:",mystery,"Type",typeof mystery);
+
+let bigNumber = 123456789123456789n;
+console.log(bigNumber * 2n);
+
+
+let symbol1 = symbol("unique");
+let symbol2 = symbol("unique");
+console.log(symbol1 === symbol2); //output: false
+
+let  toy = "car"; // put a car in the box
+console.log(toy); //output:car
+
+toy = "Truck"; // chage what's inside
+console.log(toy); // otuput: Truck
+
+var candy = "Chocolate"; //Put chocolate in the box
+console.log(candy);// output: chocolate
+
+candy = "Lollipop"; // chage what's inside
+console.log(candy); //output:Lollipop
+
+const fruits =["Apple","Banana"];
+fruits.push("Orange");
+console.log(Fruits); 
+
+//fruits = ["Grapes"];
