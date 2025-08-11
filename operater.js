259e133c205755a505a352d8d95a106f570ccc5d
PriyaMusane <priyamusane945@gmail.com>
2025-08-11 19:38:03 +0530

    Add files via upload

---
+++ operater.js
@@ -0,0 +1,63 @@
+
+
+//Arithmetic Operations//
+
+
+num1=10;
+num2=20;
+console.log("Addition",num1 + num2);
+console.log("Subtraction",num1 - num2);
+console.log("Multiplication",num1 * num2);
+console.log("Division",num1 / num2);
+console.log("Moduls",num1 % num2);
+
+//comparies Operations
+
+num1=10;
+num2=20;
+console.log("Is first grater than secound?",num1>num2);
+console.log("Is first number less than  secound?",num1<num2);
+console.log("Are the two number equl?",num1===num2);
+
+//Logical Operators//
+
+//mathScore=100;
+//scienceScore=100;
+//let mathScore=parseFloat(prompt("Enter mathScore"));
+//let scienceScore=parseFloat(prompt("Enter science score"));
+//let isEligible=(mathScore >=90, scienceScore >=85);
+//console.log("Scholarship Eligibility",isEligible)//
+
+//Assigment//
+
+ x=10;
+console.log("Initial value of x:",x);
+
+ x += 5;
+console.log("After adding 5:",x);
+
+ x*=3;
+console.log("After multiplaying by 3",x);
+
+ x-=10;
+console.log("After subtaraction",x);
+
+x/=2;
+console.log("After dividing 2:",x);
+
+x%=3;
+console.log("Modulus with 3:",x);
+
+
+// combined example//
+length=20;
+wedth=10;
+perimeter =2*(length+width);
+area=length*width;
+console.log("perimeter:",perimeter);
+console.log("Area:",area);
+console.log("Is perimeter greater then area?",perimeter > area);
+
+//Complex logical and comparison Operaties
+age=paraseInt(prompt("Enter age:"));
+isCitizen=prompt("Area you a citizen")
